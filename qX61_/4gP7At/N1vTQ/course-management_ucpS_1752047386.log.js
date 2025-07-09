73454cd60986a2071dc34d09441b4254ccdf6e77
zy49708AiR <chokers-sleeper.0x@icloud.com>
2025-07-09 03:49:52 -0400

    Enable real - time data synchronization in the file

---
+++ qX61_/4gP7At/N1vTQ/course-management_ucpS_1752047386.log.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import { PORT } from './config/env.js';
+
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js'
+import errorMiddleware from './middlewares/error.middleware.js'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
+import workflowRouter from './routes/workflow.routes.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(arcjetMiddleware);
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+app.listen(PORT, async () => {
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+  await connectToDatabase();
+});
+
+export default app;
